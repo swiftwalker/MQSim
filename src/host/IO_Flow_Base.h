da1956e577733031b43379f4f828317905a94a9b
haotuo <haotuo@haotuodeMacBook-Air.local>
2026-07-15 11:13:33 +0800

    fix: add virtual destructors to base classes to prevent SIGTRAP on macOS ARM64

--- src/host/IO_Flow_Base.h
+++ src/host/IO_Flow_Base.h
@@ -47,7 +47,7 @@ namespace Host_Components
 					 sim_time_type stop_time, double initial_occupancy_ratio, unsigned int total_requets_to_be_generated,
 					 HostInterface_Types SSD_device_type, PCIe_Root_Complex *pcie_root_complex, SATA_HBA *sata_hba,
 					 bool enabled_logging, sim_time_type logging_period, std::string logging_file_path);
-		~IO_Flow_Base();
+		virtual ~IO_Flow_Base();
 		void Start_simulation();
 		IO_Flow_Priority_Class::Priority Priority_class() { return priority_class; }
 		virtual Host_IO_Request* Generate_next_request() = 0;
